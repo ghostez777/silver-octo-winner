1491c16950995bb2bf1380bf6c48e7b16b8a7b5c
ghostez777 <22fhextall@bosworthacademy.org.uk>
2026-07-07 22:57:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -53,4 +53,17 @@ refreshBtn.addEventListener("click", () => {
 });
 
 // Fullscreen button
-fullscreen
+fullscreenBtn.addEventListener("click", () => {
+  const iframe = document.getElementById("game-frame");
+
+  if (iframe.requestFullscreen) {
+    iframe.requestFullscreen();
+  } else if (iframe.webkitRequestFullscreen) {
+    iframe.webkitRequestFullscreen();
+  } else if (iframe.msRequestFullscreen) {
+    iframe.msRequestFullscreen();
+  }
+});
+
+// Start
+loadGames();
